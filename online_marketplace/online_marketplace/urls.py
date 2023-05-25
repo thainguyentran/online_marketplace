f43c325b4c232292f9aee135cb449854d21b4e08
ttnguyen2 <thainguyentran@icloud.com>
2023-05-25 17:07:29 +0700

    add search function add clear function add edit item function add delete item function

--- online_marketplace/online_marketplace/urls.py
+++ online_marketplace/online_marketplace/urls.py
@@ -8,6 +8,7 @@ from core.views import index, contact
 urlpatterns = [
     path('', index, name='index'),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('core/', include('core.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
